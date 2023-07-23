79e593d462033df9f34cd0808796f12aa1d82eb8
LuScarton <lucasscarton@gmail.com>
2023-07-23 07:10:58 -0300

    Implemented fetch function correctly

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,6 @@
 import {useState} from 'react';
 import { Header } from '../../components/Header'
 import background from '../../assets/background.png';
-import pointerRight from '../../assets/pointerRight.png';
-import pointerLeft from '../../assets/pointerLeft.png';
 import ItemList from '../../components/ItemList';
 import './styles.css';
 
@@ -10,14 +8,38 @@ function App() {
 
   const[query, setQuery] = useState('');
   const[currentQuery, setCurrentQuery] = useState(null);
-  const[url, setUrl] = useState(null);
-  const[time, setTime] = useState(null);
-  const[results, setResults] = useState(null);
-  const[page, setPage] = useState(null);
+  const[page, setPage] = useState('1');
 
   const handleQuery = async () => {
-    const queryData = await fetch(`http://localhost:8080/v1/search?query=${query}&page=${page}`)
+    setPage('1')
+    const queryData = await fetch(`http://localhost:8080/v1/search?query=${query}&page=1`)
+    const newQuery = await queryData.json();
+
+    if(newQuery.length){
+      setCurrentQuery(newQuery);
+    } else {
+      setCurrentQuery(null);
+    }
   }
+
+  const handlePage = async (newPage) => {
+
+    if(newPage > 0){
+      setPage(newPage);
+    } else {
+      setPage('1')
+    }
+    
+    const queryData = await fetch(`http://localhost:8080/v1/search?query=${query}&page=${newPage}`);
+    const newQuery = await queryData.json();
+
+    if (newQuery.length > 0) {
+      setCurrentQuery(newQuery);
+    } else if (newQuery.length == null) {
+      setCurrentQuery(null);
+    }
+  };
+
 
   return (
     <div className="App">
@@ -31,51 +53,44 @@ function App() {
             value={query} 
             onChange={event => setQuery(event.target.value)} 
             placeholder="Digite aqui sua pesquisa e deixe que eu purr-curo pra você!" />
-            <button>Buscar</button>
+            <button className="buscar" onClick={handleQuery}>Buscar</button>
           </div>
           <hr />
-          <div>
-            <h3 className='dadosPesquisa'>Aproximadamente 100 resultados (0,31 segundos)</h3>
-            <ItemList title="teste" url="teste.com" description="teste"/>
-            <ItemList title="teste" url="teste.com" description="teste"/>
-            <ItemList title="teste" url="teste.com" description="teste"/>
-            <ItemList title="teste" url="teste.com" description="teste"/>
-            <ItemList title="teste" url="teste.com" description="teste"/>
-          </div>
-          <div className='pages'>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Me</td>
-                  <td>
-                  <div className="o-container">
-                      <div className="o">o</div>
-                      <div className="o">o</div>
-                      <div className="o">o</div>
-                      <div className="o">o</div>
-                      <div className="o">o</div>
-                      <div className="o">o</div>
-                    </div>
-                  </td>
-                  <td>w</td>
-                  <img src={pointerRight} className="pointerRight" alt="Seta para direita" />
-                </tr>
-                <tr>
-                  <td></td>
-                  <td>
-                    <div className="number-container">
-                      <div className="number">1</div>
-                      <div className="number">2</div>
-                      <div className="number">3</div>
-                      <div className="number">4</div>
-                      <div className="number">5</div>
-                      <div className="number">6</div>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          {currentQuery?.length > 0 ? (<>
+            <div>
+              <h3 className='dadosPesquisa'>Aproximadamente 100 resultados (0,31 segundos)</h3>
+              {currentQuery.map((query) => (
+                <ItemList title={query.title} url={query.url} description={query.abs}/>
+              ))}
+            </div>
+              <div className='pages'>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>Me</td>
+                      <td>
+                        <div className="o-container">
+                          <button onClick={() => handlePage(Number(page) - 1)} className="o">o</button>
+                          <button onClick={() => handlePage(Number(page))} className="o">o</button>
+                          <button onClick={() => handlePage(Number(page) + 1)} className="o">o</button>
+                        </div>
+                      </td>
+                      <td>w</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td>
+                        <div className="number-container">
+                          <div className="number">↤</div>
+                          <div className="number">{page}</div>
+                          <div className="number">↦</div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+          </>) : null}
         </div>
       </div>
     </div>
